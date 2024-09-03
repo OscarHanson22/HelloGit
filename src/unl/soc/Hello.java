89bc1934a0914c60597a68907364395e33e843b2
Ermia <Ermia@Ermias.unl.edu>
2024-09-02 20:13:04 -0500

    My Changes to Oscar's repo was printing my name (Ermias Wolde) on line 7 and line 8 the major I am enrolled (Computer Science)

--- src/unl/soc/Hello.java
+++ src/unl/soc/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Oscar Hanson");
 		System.out.println("Computer Science");
+		System.out.println("Ermias Wolde");
+		System.out.println("Computer Science");
 	}
 }
